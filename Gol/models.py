f659e8cb6c32af50a1ba00bd2c3f7f508fa889dc
Alain <shemaalain10@gmail.com>
2024-02-09 12:48:06 +0200

    Added a form, and its operations

--- Gol/models.py
+++ Gol/models.py
@@ -1,17 +1,12 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.CharField(max_length=100)
     pub_date = models.DateTimeField(auto_now_add=True)
+    state = models.CharField(max_length=100, default="visible")
 
     def __str__(self):
         return self.title
-    
-
-    
-
-
-
  
--- Gol/views.py
+++ Gol/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render
 from .models import Post
 
 def post(request):
-    posts = Post.objects.all()
-    return render(request, 'home.html' )
+    # A single line comment
+    """
+    A function for handling posts page:
+    * This function receives post requests and saves them.
+    * This function renders the page
+    """
+    if request.method == 'POST':
+        post_received = Post()
+        post_received.title = request.POST.get("title")
+        post_received.author = request.POST.get("author")
+        post_received.content = request.POST.get("content")
+        try:
+            post_received.save()
+        except:
+            return print("Error saving...")
+        
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        return render(request, 'home.html', {"posts": posts})
 
 def required(request):
     return render(request, 'REQUIREMENT.html')
@@ -13,20 +30,3 @@ def park(request):
 
 def photos(request):
     return render(request, 'PHOTOS.html')
-
-
-
-from django.urls import path
-from .views import post, required, park, photos
-
-urlpatterns = [
-    path('', post, name='post'),
-    path('requirements/', required, name='requirement'),
-    path('photos/', photos, name='photos'),
-    path('park/', park, name='park'),
-]
-
-
-
-
-
